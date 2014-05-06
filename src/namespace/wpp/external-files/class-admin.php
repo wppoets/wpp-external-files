837aa6eb478398c6a546904e317f512f64daf9d3
Michael Stutz <michaeljstutz@gmail.com>
2014-05-06 16:02:59 -0400

    Remove unneeded nested if

--- src/namespace/wpp/external-files/class-admin.php
+++ src/namespace/wpp/external-files/class-admin.php
@@ -101,64 +101,65 @@ class Admin extends \WPP\External_Files\Base\Admin {
 					$matched_url = array();
 					if ( preg_match( static::PREG_EXTRACT_URL, $element, $matched_url ) ) {
 						$url = empty( $matched_url[2] ) ? NULL : $matched_url[2];
-						if ( ! empty( $url ) ) {
-							if ( empty( $wp_options[ 'import_extensions' ] ) ) {
-								continue;
-							}
-							$url_parts = parse_url( $url );
-							if ( empty( $url_parts['path'] ) ) {
-								continue;
-							}
-							$path_parts = pathinfo( $url_parts['path'] );
-							if ( empty( $path_parts['extension'] ) ) {
-								continue;
-							}
-							// Check to see if it is a valid file extentions
-							if ( ! static::custom_find_match( explode( PHP_EOL, $wp_options[ 'import_extensions' ] ), $path_parts['extension'] ) ) {
-								continue; // If we didnt find a matching extention skip url
-							}
-							// Make sure it is not an excluded url
-							$excluded_urls = explode( PHP_EOL, $wp_options[ 'excluded_urls' ] );
-							$home_url_parts = parse_url( home_url() );
-							$excluded_urls[] = '//' . $home_url_parts[ 'host' ]; // Add the current hostname of the home_url()
-							if ( static::custom_find_match( $excluded_urls, $url ) ) {
-								continue; // If we didnt find a matching extention skip url
-							}
-							$attachment_id = static::find_attachment_id_by_external_url( $url );
-							if ( empty( $attachment_id ) ) { // We cant find it so we need to add it
-								defined('STDIN') or set_time_limit( static::PHP_SET_TIME_LIMIT ); //If we are not running from the command line change the time limit
-								$tmp_name = apply_filters( $options[ 'wp_filter_pre_tag' ] . 'tmp_name_download_url', NULL, $url );
-								if ( empty( $tmp_name ) ) {
-									$tmp_name = download_url( $url );
-								}
-								$file_array = array(
-									'name' => basename( $url_parts['path'] ),
-									'tmp_name' => $tmp_name,
-								);
-								// If error storing temporarily, unlink
-								if ( is_wp_error( $tmp_name ) ) {
-									@unlink( $file_array[ 'tmp_name' ] );
-									$file_array[ 'tmp_name' ] = '';
-								}
-								$attachment_id = media_handle_sideload( $file_array, $post_id, '' );
-								// If error storing permanently, unlink
-								if ( is_wp_error( $attachment_id ) ) {
-									@unlink( $file_array['tmp_name'] );
-									$attachment_id = NULL;
-								} else {
-									add_post_meta( $attachment_id, $options[ 'metadata_key_external_url' ], $url, TRUE ) or update_post_meta( $attachment_id, $options[ 'metadata_key_external_url' ], $url );
-								}
-							}
-							if ( empty ( $attachment_id ) ) {
-								continue; // Something went wrong, just skip
-							}
-							$attachment_url = wp_get_attachment_url( $attachment_id );
-							if ( empty ( $attachment_url ) ) {
-								continue; // Something went wrong, just skip
-							}
-							$new_element = str_replace( $url, $attachment_url, $element );
-							$content = str_replace( $element, $new_element, $content );
+						if ( empty( $url ) ) {
+							continue;
 						}
+						if ( empty( $wp_options[ 'import_extensions' ] ) ) {
+							continue;
+						}
+						$url_parts = parse_url( $url );
+						if ( empty( $url_parts['path'] ) ) {
+							continue;
+						}
+						$path_parts = pathinfo( $url_parts['path'] );
+						if ( empty( $path_parts['extension'] ) ) {
+							continue;
+						}
+						// Check to see if it is a valid file extentions
+						if ( ! static::custom_find_match( explode( PHP_EOL, $wp_options[ 'import_extensions' ] ), $path_parts['extension'] ) ) {
+							continue; // If we didnt find a matching extention skip url
+						}
+						// Make sure it is not an excluded url
+						$excluded_urls = explode( PHP_EOL, $wp_options[ 'excluded_urls' ] );
+						$home_url_parts = parse_url( home_url() );
+						$excluded_urls[] = '//' . $home_url_parts[ 'host' ]; // Add the current hostname of the home_url()
+						if ( static::custom_find_match( $excluded_urls, $url ) ) {
+							continue; // If we didnt find a matching extention skip url
+						}
+						$attachment_id = static::find_attachment_id_by_external_url( $url );
+						if ( empty( $attachment_id ) ) { // We cant find it so we need to add it
+							defined('STDIN') or set_time_limit( static::PHP_SET_TIME_LIMIT ); //If we are not running from the command line change the time limit
+							$tmp_name = apply_filters( $options[ 'wp_filter_pre_tag' ] . 'tmp_name_download_url', NULL, $url );
+							if ( empty( $tmp_name ) ) {
+								$tmp_name = download_url( $url );
+							}
+							$file_array = array(
+								'name' => basename( $url_parts['path'] ),
+								'tmp_name' => $tmp_name,
+							);
+							// If error storing temporarily, unlink
+							if ( is_wp_error( $tmp_name ) ) {
+								@unlink( $file_array[ 'tmp_name' ] );
+								$file_array[ 'tmp_name' ] = '';
+							}
+							$attachment_id = media_handle_sideload( $file_array, $post_id, '' );
+							// If error storing permanently, unlink
+							if ( is_wp_error( $attachment_id ) ) {
+								@unlink( $file_array['tmp_name'] );
+								$attachment_id = NULL;
+							} else {
+								add_post_meta( $attachment_id, $options[ 'metadata_key_external_url' ], $url, TRUE ) or update_post_meta( $attachment_id, $options[ 'metadata_key_external_url' ], $url );
+							}
+						}
+						if ( empty ( $attachment_id ) ) {
+							continue; // Something went wrong, just skip
+						}
+						$attachment_url = wp_get_attachment_url( $attachment_id );
+						if ( empty ( $attachment_url ) ) {
+							continue; // Something went wrong, just skip
+						}
+						$new_element = str_replace( $url, $attachment_url, $element );
+						$content = str_replace( $element, $new_element, $content );
 					}
 				}
 			}
